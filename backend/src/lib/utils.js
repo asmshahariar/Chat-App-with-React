f09e57037b335ecb29ea0fa981e19975a1c708dc
asmshahariar <shahariar.arian@gmail.com>
2025-11-19 14:07:29 +0600

    welcome email implemented

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (res, userId) => {
 
-  const { JWT_SECRET } = process.env;
+  const { JWT_SECRET } = ENV;
   if (!JWT_SECRET) {
     throw new Error("JWT_SECRET is not defined");
   }
@@ -13,7 +14,7 @@ export const generateToken = (res, userId) => {
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
     httpOnly: true, //prevent XSS attacks: cross-site scripting
     sameSite: "strict", //prevent CSRF attacks: cross-site request forgery
-    secure: process.env.NODE_ENV === "development" ? false : true, 
+    secure: ENV.NODE_ENV === "development" ? false : true, 
   })
 
   return token;
