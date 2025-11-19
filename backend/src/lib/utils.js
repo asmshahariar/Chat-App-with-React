ca54e25621e52042380d2e704a26200ce246b555
asmshahariar <shahariar.arian@gmail.com>
2025-11-19 13:23:24 +0600

    signup code updated

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,8 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (res, userId) => {
+
+  const { JWT_SECRET } = process.env;
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined");
+  }
   
-  const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: "15d"})
+  const token = jwt.sign({userId}, JWT_SECRET, {expiresIn: "15d"})
   
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
